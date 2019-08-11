b9e80cc1bbf142a4410d80b27ef473c196a60c58
Kaan Ozdemir <kaan.ozdemir@tsoft.com.tr>
2019-08-11 20:38:23 +0300

    Initial Commit

---
+++ FlickrTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrTestApp
+//
+//  Created by Kaan Ozdemir on 11.08.2019.
+//  Copyright © 2019 Kaan Ozdemir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
